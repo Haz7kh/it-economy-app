6090cac81c2f9d15443317d42137cad7c4612268
Khaled <kh.almelhem@gmail.com>
2024-05-10 15:03:33 +0200

    fixed the toggle menu btn

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
 
 const Header = () => {
+  // State to manage the collapse state of the navbar
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  // Function to toggle the collapse state of the navbar
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <nav className="navbar navbar-expand-lg ">
       <div className="container justify-content-between">
         <Link className="navbar-brand brand" to="/">
           IT & Economy App
         </Link>
+        {/* Toggle button */}
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
           aria-controls="navbarNav"
-          aria-expanded="false"
+          aria-expanded={!isNavCollapsed ? true : false}
           aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
         >
           <span className="navbar-toggler-icon toggler-icon"></span>
         </button>
+        {/* Close button (only appears when menu is open) */}
+        {!isNavCollapsed && (
+          <button
+            className="close close-btn btn"
+            type="button"
+            aria-label="Close"
+            onClick={handleNavCollapse}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        )}
+        {/* Navbar links */}
         <div
-          className="collapse navbar-collapse justify-content-center"
+          className={`${
+            isNavCollapsed ? "collapse" : ""
+          } navbar-collapse justify-content-center`}
           id="navbarNav"
         >
           <ul className="navbar-nav">
